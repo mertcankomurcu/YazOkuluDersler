b622ce08d3f7c7f5d1641b0a02d5f9f094f68fda
mertcankomurcu <mertcankomurcu@outlook.com.tr>
2024-03-29 04:10:53 +0300

    Başarıyla tamamlandı.

---
+++ DataAccessLayer/Baglanti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection bgl = new SqlConnection(@"Data Source=LAPTOP-6OCSKQ8T;Initial Catalog=DbYazOkulu;Integrated Security=True;");
+    }
+}
